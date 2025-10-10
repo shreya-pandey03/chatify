742f7140f497b95b7c35ea7e8fcc209e98c0c513
shreya-pandey03 <reachatpandey@gmail.com>
2025-10-10 22:50:56 +0530

    rate-limiting implemented

--- backened/src/lib/env.js
+++ backened/src/lib/env.js
@@ -1,12 +1,16 @@
 import "dotenv/config";
 
-export const PORT = process.env.PORT || 5000;       
-export const MONGO_URL = process.env.MONGO_URL;
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const Node_ENV = process.env.NODE_ENV || "development";
-export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
-export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
-export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
-export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
-export const EMAIL_USER = process.env.EMAIL_USER;
-export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+export const ENV={
+ PORT : process.env.PORT || 5000,     
+ MONGO_URL : process.env.MONGO_URL,
+ JWT_SECRET : process.env.JWT_SECRET,
+ Node_ENV : process.env.NODE_ENV || "development",
+ CLOUDINARY_CLOUD_NAME : process.env.CLOUDINARY_CLOUD_NAME,
+ CLOUDINARY_API_KEY : process.env.CLOUDINARY_API_KEY,
+ CLOUDINARY_API_SECRET : process.env.CLOUDINARY_API_SECRET,
+ CLIENT_URL : process.env.CLIENT_URL || "http://localhost:3000",
+ EMAIL_USER : process.env.EMAIL_USER,
+ EMAIL_PASSWORD : process.env.EMAIL_PASSWORD,
+ ARCJET_KEY : process.env.ARCJET_KEY,
+ ARCJET_ENV : process.env.ARCJET_ENV || "development",
+};
