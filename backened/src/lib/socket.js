3f8a399af2fe5b9deb3e44c7f9e3a8792a8e7c8d
shreya-pandey03 <reachatpandey@gmail.com>
2025-10-20 12:53:36 +0530

    online user implemented

--- backened/src/lib/socket.js
+++ backened/src/lib/socket.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 import { ENV } from "./env.js";
-// import { socketAuthMiddleware } from "../middleware/socket.auth.middleware.js";
+import { socketAuthMiddleware } from "../middleware/socket.auth.middleware.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -15,31 +15,31 @@ const io = new Server(server, {
 });
 
 // apply authentication middleware to all socket connections
-// io.use(socketAuthMiddleware);
+io.use(socketAuthMiddleware);
 
 // we will use this function to check if the user is online or not
-// export function getReceiverSocketId(userId) {
-//   return userSocketMap[userId];
-// }
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
 
-// // this is for storig online users
-// const userSocketMap = {}; // {userId:socketId}
+// this is for storig online users
+const userSocketMap = {}; // {userId:socketId}
 
-// io.on("connection", (socket) => {
-//   console.log("A user connected", socket.user.fullName);
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.user.fullName);
 
-//   const userId = socket.userId;
-//   userSocketMap[userId] = socket.id;
+  const userId = socket.userId;
+  userSocketMap[userId] = socket.id;
 
-//   // io.emit() is used to send events to all connected clients
-//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  // io.emit() is used to send events to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-//   // with socket.on we listen for events from clients
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected", socket.user.fullName);
-//     delete userSocketMap[userId];
-//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-//   });
-// });
+  // with socket.on we listen for events from clients
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.user.fullName);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 export { io, app, server };
