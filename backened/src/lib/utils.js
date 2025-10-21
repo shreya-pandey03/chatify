a357de5c1aa9b0a81644b3b3496f06cf9219c946
shreya-pandey03 <reachatpandey@gmail.com>
2025-10-21 11:50:06 +0530

    backend setup

--- backened/src/lib/utils.js
+++ backened/src/lib/utils.js
@@ -1,25 +1,14 @@
-import jwt from "jsonwebtoken";
-import { ENV } from "./env.js";
+import jwt from 'jsonwebtoken';
 
-export const generateToken = (userId, res) => {
-  const { JWT_SECRET } = ENV;
-  if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET is not configured");
-  }
-
-  const token = jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: "7d",
+export const generateToken = (user) => {
+    return jwt.sign({id:user._id},process.env.JWT_SECRET,{
+        expiresIn:"7d",
+    });
+  res.cookie("jwt",token,{
+    maxAge:7*24*60*60*1000,
+    httpOnly:true,
+    sameSite:"stric" ,
+    secure:process.env.NODE_ENV==="development"?false:true,
   });
-
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
-  });
-
-  return token;
+    return token;
 };
-
-// http://localhost
-// https://dsmakmk.com
